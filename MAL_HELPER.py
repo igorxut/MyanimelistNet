332c69e5c78907222875790c3eefce244919a93f
igorxut <igorxut@gmail.com>
2016-12-05 13:12:06 +0300

    update

--- MAL_HELPER.py
+++ MAL_HELPER.py
@@ -11,17 +11,13 @@ from urllib.request import urlopen
 
 def write_log(f_file, f_text):
 
-    with open(f_file, "ab") as f:
-        f.write(f_text.encode("utf8"))
-        f.write("\n".encode("utf8"))
+    f_file.write(f_text)
+    f_file.write("\n")
 
 
-def get_list(f_list, f_user, f_site, f_file):
+def get_list(f_list, f_user, f_site, f_file_errors, f_file_results):
 
-    for item_type in (
-            "anime",
-            "manga",
-    ):
+    for item_type in ("anime", "manga"):
 
         link = "{0}/malappinfo.php?u={1}&status=all&type={2}".format(
             f_site,
@@ -34,7 +30,7 @@ def get_list(f_list, f_user, f_site, f_file):
             datetime.now().time(),
             link,
         )
-        print(log_text,)
+        print(log_text)
 
         for item in root_xml.iter("series_{0}db_id".format(item_type,)):
 
@@ -48,40 +44,38 @@ def get_list(f_list, f_user, f_site, f_file):
                     item_type,
                     item_id,
                 )
-                write_log(f_file, log_text)
+                write_log(f_file_errors, log_text)
 
-            f_list.append((item_type, item_id,))
+            f_list.append((item_type, item_id))
             log_text = "{0}\tadded {1}_id={2} to list.".format(
                 datetime.now().time(),
                 item_type,
                 item_id,
             )
-            print(log_text,)
+            print(log_text)
 
         log_text = "{0}\tparsing of {1} list is complete.".format(
             datetime.now().time(),
             item_type,
         )
-        print(log_text,)
+        print(log_text)
 
     log_text = "{0}\tgetting list is complete.".format(
         datetime.now().time(),
     )
-    print(log_text,)
+    print(log_text)
 
     return f_list
 
 
-def get_page(f_site, f_task, f_file):
+def get_page(f_site, f_task, f_file_errors, f_file_results):
 
     link = "{0}/{1}/{2}/".format(
         f_site,
         f_task[0],
         f_task[1],
     )
-    page = requests.get(
-        link
-    )
+    page = requests.get(link)
 
     # error 404 (not found)
     if page.status_code == 404:
@@ -89,13 +83,8 @@ def get_page(f_site, f_task, f_file):
             datetime.now().time(),
             link,
         )
-        print(
-            log_text,
-        )
-        write_log(
-            f_file,
-            log_text
-        )
+        print(log_text)
+        write_log(f_file_errors, log_text)
         return None
 
     # error 429 (too many requests)
@@ -107,10 +96,7 @@ def get_page(f_site, f_task, f_file):
                 datetime.now().time(),
                 link,
             )
-            write_log(
-                f_file,
-                log_text
-            )
+            write_log(f_file_errors, log_text)
             break
 
         counter += 1
@@ -118,42 +104,27 @@ def get_page(f_site, f_task, f_file):
             datetime.now().time(),
             link,
         )
-        print(
-            log_text,
-        )
-        time.sleep(
-            5
-        )
-        page = requests.get(
-            link
-        )
+        print(log_text)
+        time.sleep(5)
+        page = requests.get(link)
 
     return page
 
 
-def parse_page(
-        f_page,
-        f_file
-):
+def parse_page(f_page, f_file_errors, f_file_results):
 
     root_html = html.fromstring(
         f_page.content
     )
     f_set = set()
 
-    for attr in root_html.xpath(
-            '//table[@class="anime_detail_related_anime"]//a/@href'
-    ):
+    for attr in root_html.xpath('//table[@class="anime_detail_related_anime"]//a/@href'):
 
-        f_list = attr.split(
-            '/'
-        )
+        f_list = attr.split("/")
         item_type = f_list[1]
 
         try:
-            item_id = int(
-                f_list[2]
-            )
+            item_id = int(f_list[2])
 
         except ValueError:
 
@@ -166,18 +137,10 @@ def parse_page(
                 item_type,
                 item_id,
             )
-            write_log(
-                f_file,
-                log_text
-            )
+            write_log(f_file_errors, log_text)
 
-        task = (
-            item_type,
-            item_id,
-        )
-        f_set.add(
-            task
-        )
+        task = (item_type, item_id)
+        f_set.add(task)
 
     return f_set
 
@@ -190,31 +153,27 @@ def main(user):
     visited_set = set()
     queue = deque()
 
-    file_result = "result.tsv"
     file_error_log = "errors.tsv"
+    file_result = "result.tsv"
 
-    result_file = open(
-        file_result,
-        "wb"
-    )
-    result_file.close()
-    error_log_file = open(
-        file_error_log,
-        "wb"
-    )
+    error_log_file = open(file_error_log, "w")
     error_log_file.close()
+    result_file = open(file_result, "w")
+    result_file.close()
+
+    error_log_file = open(file_error_log, "a")
+    result_file = open(file_result, "a")
 
     user_list.extend(
         get_list(
             user_list,
             user,
             site,
-            file_error_log
+            error_log_file,
+            result_file
         )
     )
-    queue.extend(
-        user_list
-    )
+    queue.extend(user_list)
 
     while queue:
 
@@ -228,63 +187,48 @@ def main(user):
                 datetime.now().time(),
                 task,
             )
-            print(
-                log_text
-            )
+            print(log_text)
             log_text = "{0}\t{1}\t{2}/{0}/{1}/".format(
                 task[0],
                 task[1],
                 site,
             )
-            write_log(
-                file_result,
-                log_text
-            )
+            write_log(result_file, log_text)
 
         visited_set.add(task)
         log_text = "{0}\t{1} added to visited_set.".format(
             datetime.now().time(),
             task,
         )
-        print(
-            log_text,
-        )
+        print(log_text)
 
-        page = get_page(
-            site,
-            task,
-            file_error_log
-        )
+        page = get_page(site, task, error_log_file, result_file)
 
         if page is None:
             continue
 
         else:
-            urls_set = parse_page(
-                page,
-                file_error_log
-            )
+            urls_set = parse_page(page, error_log_file, result_file)
 
         for item in urls_set:
 
             if item not in visited_set:
-                queue.append(
-                    item
-                )
+                queue.append(item)
                 log_text = "{0}\t{1} added to queue.".format(
                     datetime.now().time(),
                     item,
                 )
-                print(
-                    log_text
-                )
+                print(log_text)
 
     log_text = "{0}\tqueue is empty, mission complete.".format(
-        datetime.now().time()
+        datetime.now().time(),
     )
     print(log_text)
+
+    result_file.close()
+    error_log_file.close()
 
     return None
 
 if __name__ == "__main__":
-    main("XuT")
+    main("user")
